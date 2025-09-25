2b79e1797da56828922e013bee5a81d983e33046
andre <andrea.marind.1@gmail.com>
2025-09-25 01:47:03 -0500

    Eliminar carpeta frontend desorganizada

--- backend/src/main/java/com/udea/bancoudea/BancoudeaApplication.java
+++ backend/src/main/java/com/udea/bancoudea/BancoudeaApplication.java
@@ -3,7 +3,7 @@ package com.udea.bancoudea;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "com.udea.bancoudea")
+@SpringBootApplication()
 public class BancoudeaApplication {
 
 	public static void main(String[] args) {
